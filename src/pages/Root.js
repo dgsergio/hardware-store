a5991630d65ab9178bd0e33161a4a3baa87dec88
Sergio Garofalo <dg.sergio@gmail.com>
2023-04-17 13:33:33 +0200

    remove url db

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -20,21 +20,3 @@ const Root = () => {
 };
 
 export default Root;
-
-export const loader = async () => {
-  try {
-    const response = await fetch(
-      'https://hardware-store-93ec6-default-rtdb.firebaseio.com/products.json'
-    );
-    if (!response.ok)
-      throw new Error('Fail to resolve the request error: ' + response.status);
-    const data = await response.json();
-    let dataArray = [];
-    for (let id in data) {
-      dataArray = [...dataArray, { id, ...data[id] }];
-    }
-    return dataArray;
-  } catch (err) {
-    return { error: true, message: 'Fail to fetch data.' };
-  }
-};
